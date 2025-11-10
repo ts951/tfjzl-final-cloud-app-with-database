00938b009c3cbbbfbf67f983cbbe082bd96cde55
Thomas Squires <thomassquires951@gmail.com>
2025-11-10 16:58:41 +0000

    Register model changes in admin.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -117,3 +117,4 @@ class Choice(models.Model):
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
+
